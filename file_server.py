474b7d463cdd6541f808dfdc0560de77f0b8a6e9
Jayne1111 <53257562+Jayne1111@users.noreply.github.com>
2019-09-27 23:32:20 +0800

    更新

--- file_server.py
+++ file_server.py
@@ -6,6 +6,14 @@ import sys
 import os
 import threading
 import hashlib
+import json
+import user_reg_login
+
+
+conf = json.load(open("server_conf.json"))  # 加载配置信息
+dest_file_abs_path = os.path.abspath(sys.argv[1])
+dest_file_parent_path = os.path.dirname(dest_file_abs_path)
+dest_file_name = os.path.basename(dest_file_abs_path)
 
 
 def get_file_md5(file_path):
@@ -72,34 +80,95 @@ def send_empty_dir(sock_conn, dir_abs_path):
     sock_conn.send(file_desc_info)
 
 
-def user_server_thread(sock_conn):
-    try:
-        for root, dirs, files in os.walk(dest_file_abs_path):
-            if len(dirs) == 0 and len(files) == 0:
-                send_empty_dir(sock_conn, root)
-                continue
+def send_dir(sock_conn):
+    '''
+    发送非空文件夹
+    '''
+    for root, dirs, files in os.walk(dest_file_abs_path):
+        if len(dirs) == 0 and len(files) == 0:
+            send_empty_dir(sock_conn, root)
+            continue
 
-            for f in files:
-                file_abs_path = os.path.join(root, f)
-                print(file_abs_path)
-                send_one_file(sock_conn, file_abs_path)
-    except Exception as e:
-        print(e)
+        for f in files:
+            file_abs_path = os.path.join(root, f)
+            print(file_abs_path)
+            send_one_file(sock_conn, file_abs_path)
+
+
+def user_service_thread(sock_conn):
+    try:
+        data_len = sock_conn.recv(15).decode().rstrip()
+        if len(data_len) > 0:
+            data_len = int(data_len)
+
+            recv_size = 0
+            json_data = b""
+            while recv_size < data_len:
+                tmp = sock_conn.recv(data_len - recv_size)
+                if tmp == 0:
+                    break
+                json_data += tmp
+                recv_size += len(tmp)
+            
+            json_data = json_data.decode()
+            req = json.loads(json_data)
+
+            if req["op"] == 1:
+                # 登录校验
+                rsp = {"op": 1, "error_code": 0}
+
+                if user_reg_login.check_uname_pwd(req["args"]["uname"], req["args"]["passwd"]):
+                    rsp["error_code"] = 1
+                
+                header_data = json.dumps(rsp).encode()
+                data_len = "{:<15}".format(len(header_data)).encode()
+                sock_conn.send(data_len)
+                sock_conn.send(header_data)
+
+                if not rsp["error_code"]:
+                    send_dir(sock_conn)
+
+            elif req["op"] == 2:
+                # 用户注册
+                rsp = {"op": 2, "error_code": 0}
+                if not user_reg_login.user_reg(req["args"]["uname"], req["args"]["passwd"], req["args"]["phone"], req["args"]["email"]):
+                    # 注册失败
+                    rsp["error_code"] = 1
+
+                rsp = json.dumps(rsp).encode()
+                data_len = "{:<15}".format(len(rsp)).encode()
+                sock_conn.send(data_len)
+                sock_conn.send(rsp)            
+
+            elif req["op"] == 3:
+                # 校验用户名是否存在
+                rsp = {"op": 3, "error_code": 0}
+
+                ret = user_reg_login.check_user_name(req["args"]["uname"])
+                if ret == 2:
+                    rsp["error_code"] = 1
+                
+                rsp = json.dumps(rsp).encode()
+                data_len = "{:<15}".format(len(rsp)).encode()
+                sock_conn.send(data_len)
+                sock_conn.send(rsp)            
     finally:
         sock_conn.close()
 
-dest_file_abs_path = os.path.abspath(sys.argv[1])
-dest_file_parent_path = os.path.dirname(dest_file_abs_path)
-dest_file_name = os.path.basename(dest_file_abs_path)
+
+def main():
+    sock_listen = socket.socket()
+    sock_listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock_listen.bind((conf["app_server_ip"], conf["app_server_port"]))
+    sock_listen.listen(5)
+
+    while True:
+        sock_conn, client_addr = sock_listen.accept()
+        print(client_addr, "已连接！")
+        threading.Thread(target=user_service_thread, args=(sock_conn, )).start()
+
+    sock_listen.close()
 
 
-sock_listen = socket.socket()
-sock_listen.bind(("0.0.0.0", 9999))
-sock_listen.listen(5)
-
-while True:
-    sock_conn, client_addr = sock_listen.accept()
-    print(client_addr, "已连接！")
-    threading.Thread(target=user_server_thread, args=(sock_conn, )).start()
-
-sock_listen.close()
+if __name__ == '__main__':
+    main()
